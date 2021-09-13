04990f0fdb3c864b72c3e4ecb1a94f90bb6e42fc
Yoselin-Arvelaiz7 <yoselin.arvelaiz7@gmail.com>
2021-09-13 12:15:01 -0400

    cambios

--- proof/_init_.py
+++ proof/_init_.py
@@ -5,5 +5,4 @@ Created on Mon Sep 13 11:40:39 2021
 @author: ar_fe
 """
 
-def probando():
-    print('Que vaina con esto')
+from proof.funcion import probando
---
+++ proof/funcion.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 13 12:11:56 2021
+
+@author: ar_fe
+"""
+
+def probando():
+    print('Que vaina con esto')
