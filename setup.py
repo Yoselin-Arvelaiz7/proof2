74238eaa9112d8366cc49226e75e21a4e597d98c
Yoselin-Arvelaiz7 <yoselin.arvelaiz7@gmail.com>
2021-09-13 11:50:28 -0400

    hola

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 13 11:45:57 2021
+
+@author: ar_fe
+"""
+
+from setuptools import setup
+
+setup(
+    name='proof',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='git@github.com:Yoselin-Arvelaiz7/proof2.git',
+    author='Yoselin Arvelaiz',
+    author_email='yoselin.arvelaiz7@gmail.com',
+    license='unlicense',
+    packages=['proof'],
+    zip_safe=False
+)
